bfd604d666c589d75c9e9a5aed81775b61087dcf
Tim Smith <tsmith84@gmail.com>
2015-09-11 11:15:30 -0700

    Resolve rubocop warnings

--- files/default/test/default_test.rb
+++ files/default/test/default_test.rb
@@ -3,9 +3,9 @@ describe_recipe 'apparmor' do
     `/usr/sbin/service apparmor status 2>&1`
 
     if node['apparmor']['disable']
-      assert_equal 2, $?.exitstatus
+      assert_equal 2, $CHILD_STATUS.exitstatus
     else
-      assert_equal 0, $?.exitstatus
+      assert_equal 0, $CHILD_STATUS.exitstatus
     end
   end
 
@@ -13,7 +13,7 @@ describe_recipe 'apparmor' do
     if node['apparmor']['disable']
       output = `dpkg -l apparmor | grep ii 2>&1`
       assert_match /^$/, output # rubocop: disable Lint/AmbiguousRegexpLiteral
-      assert_equal 1, $?.exitstatus
+      assert_equal 1, $CHILD_STATUS.exitstatus
     else
       package('apparmor').must_be_installed
     end
