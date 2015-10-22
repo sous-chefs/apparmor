f6ae22ed0849ba8d8e0739e1969b66d9f8b1da56
Tim Smith <tsmith84@gmail.com>
2015-10-22 12:59:03 -0700

    Convert the LWRP to a custom resource

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Disables apparmor service on Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 supports 'ubuntu'
+depends 'compat_resource'
+
 recipe 'apparmor::default', 'Disables apparmor service on Ubuntu'
 
 source_url 'https://github.com/chef-cookbooks/apparmor' if respond_to?(:source_url)
--- resources/policy.rb
+++ resources/policy.rb
@@ -16,9 +16,34 @@
 # See the License for the specific language governing permissions and
 #
 
-actions :add, :remove
-default_action :add
+property :name, kind_of: String, name_attribute: true
+property :source_cookbook, kind_of: String
+property :source_filename, kind_of: String
 
-attribute :name, kind_of: String, name_attribute: true
-attribute :source_cookbook, kind_of: String
-attribute :source_filename, kind_of: String
+action :add do
+  cookbook_file "/etc/apparmor.d/#{new_resource.name}" do
+    cookbook new_resource.source_cookbook if new_resource.source_cookbook
+    source new_resource.source_filename if new_resource.source_filename
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :reload, 'service[apparmor]', :immediately
+  end
+
+  service 'apparmor' do
+    supports status: true, restart: true, reload: true
+    action [:nothing]
+  end
+end
+
+action :remove do
+  file "/etc/apparmor.d/#{new_resource.name}" do
+    action :delete
+    notifies :reload, 'service[apparmor]', :immediately
+  end
+
+  service 'apparmor' do
+    supports status: true, restart: true, reload: true
+    action [:nothing]
+  end
+end
