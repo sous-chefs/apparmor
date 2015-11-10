71f4eb48bc1ce4387d07717f2ca2c370050c71d1
Tim Smith <tsmith84@gmail.com>
2015-11-10 13:51:50 -0800

    Fix the custom resource variable usage

--- resources/policy.rb
+++ resources/policy.rb
@@ -16,14 +16,14 @@
 # See the License for the specific language governing permissions and
 #
 
-property :name, kind_of: String, name_attribute: true
-property :source_cookbook, kind_of: String
-property :source_filename, kind_of: String
+property :name, String, name_attribute: true
+property :source_cookbook, String
+property :source_filename, String
 
 action :add do
-  cookbook_file "/etc/apparmor.d/#{new_resource.name}" do
-    cookbook new_resource.source_cookbook if new_resource.source_cookbook
-    source new_resource.source_filename if new_resource.source_filename
+  cookbook_file "/etc/apparmor.d/#{name}" do
+    cookbook source_cookbook if source_cookbook
+    source source_filename if source_filename
     owner 'root'
     group 'root'
     mode '0644'
@@ -37,7 +37,7 @@ action :add do
 end
 
 action :remove do
-  file "/etc/apparmor.d/#{new_resource.name}" do
+  file "/etc/apparmor.d/#{name}" do
     action :delete
     notifies :reload, 'service[apparmor]', :immediately
   end
