8c64a651e7d1659d232bedf09bb9b4d652004b6e
Tim Smith <tsmith@chef.io>
2016-09-15 15:31:58 -0700

    Depend on compat_resource 12.14+ Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ description 'Disables apparmor service on Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
 supports 'ubuntu'
-depends 'compat_resource'
+depends 'compat_resource', '>= 12.14'
 
 recipe 'apparmor::default', 'Disables apparmor service on Ubuntu'
 
 source_url 'https://github.com/chef-cookbooks/apparmor'
 issues_url 'https://github.com/chef-cookbooks/apparmor/issues'
 
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1'
