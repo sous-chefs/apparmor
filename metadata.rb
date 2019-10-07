7635f925d7eed95ab5ce114aa1408fca47cf0ec2
Tim Smith <tsmith84@gmail.com>
2019-10-07 16:17:22 -0700

    Resolve Cookstyle 5.8 warnings
    Fix it up
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,8 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Disables apparmor service on Ubuntu'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.1.0'
 supports 'ubuntu'
-
-recipe 'apparmor::default', 'Disables apparmor service on Ubuntu'
 
 source_url 'https://github.com/chef-cookbooks/apparmor'
 issues_url 'https://github.com/chef-cookbooks/apparmor/issues'
