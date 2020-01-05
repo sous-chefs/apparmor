0489efadeea7cc46fdf6c1bbd5a829570a3854a9
Tim Smith <tsmith84@gmail.com>
2020-01-05 11:52:39 -0800

    Require Chef 12.15+
    This gives us modern metadata parsing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,4 @@ supports 'ubuntu'
 
 source_url 'https://github.com/chef-cookbooks/apparmor'
 issues_url 'https://github.com/chef-cookbooks/apparmor/issues'
-chef_version '>= 12.7'
+chef_version '>= 12.15'
