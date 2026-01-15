6969c6161a0ac2c90fecc0646aafac5c12b6e227
Dan Webb <dan.webb@damacus.io>
2026-01-15 10:36:54 +0000

    fix: remove EOL platforms and update to supported versions (#66)
    * fix: remove EOL platforms and update to supported versions
    - Remove EOL Ubuntu: 18.04, 20.04, 23.04
    - Remove EOL Debian: 9, 10, 11
    - Remove non-Ubuntu/Debian platforms (cookbook only supports these)
    - Support: Ubuntu 22.04, 24.04, Debian 12, 13
    - Add Debian to metadata.rb
    
    * Run tests in dokken?
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>
    
    * fix: skip kernel module check in Docker containers
    
    Kernel module parameters at /sys/module/apparmor/parameters/enabled
    are not accessible in Docker containers since they share the host kernel.
    
    * fix: use bare metal runners for proper AppArmor testing
    
    - Update CI to run on ubuntu-22.04/24.04 bare metal runners
    - Use kitchen.exec.yml instead of Dokken for kernel module access
    - Revert InSpec Docker skip - now testing real kernel params
    - Add Taskfile for local testing without Dokken
    
    * Update workflows
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>
    
    * Ignore that folder
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>
    
    * fix: update InSpec tests for realistic verification
    
    - Check GRUB config instead of kernel module (requires reboot)
    - Remove include_controls from add/remove suites
    - Add explicit AppArmor enabled checks to add/remove tests
    - Skip service enabled check when disabled (unreliable on Debian)
    - Fix Taskfile to unset KITCHEN_LOCAL_YAML properly
    
    * fix: configure kitchen.exec.yml for bare-metal CI
    
    - Add provisioner and verifier config for exec driver
    - Set platforms to ubuntu-22.04 and ubuntu-24.04
    - Update CI workflow to use kitchen.exec.yml
    
    * VAGRANT
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>
    
    ---------
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/remove/controls/verify_added.rb
+++ test/integration/remove/controls/verify_added.rb
@@ -1,6 +1,13 @@
-include_controls 'apparmor-common'
-
 control 'apparmor-remove' do
+  describe package 'apparmor' do
+    it { should be_installed }
+  end
+
+  describe service 'apparmor' do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
   describe command('apparmor_status') do
     its('stdout') { should_not match %r{/usr/sbin/my_policy} }
   end
