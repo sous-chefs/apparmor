0b87e4485237582349bd84d2b38e1ade71443fe7
Dan Webb <dan.webb@damacus.io>
2026-03-14 19:56:33 +0000

    test: add InSpec profiles and update kitchen configuration
    - Add default, disable, policy-add, policy-remove InSpec suites
    
    - Rewrite kitchen.yml with YAML anchors and 4 new suites
    
    - Remove debian-13 from kitchen.dokken.yml to match kitchen.yml

---
+++ test/integration/policy-remove/controls/policy_remove_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+title 'Policy remove suite'
+
+control 'apparmor-policy-remove-01' do
+  impact 1.0
+  title 'AppArmor is installed and running'
+
+  describe package('apparmor') do
+    it { should be_installed }
+  end
+
+  describe service('apparmor') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
+
+control 'apparmor-policy-remove-02' do
+  impact 1.0
+  title 'Policy file is removed'
+
+  describe file('/etc/apparmor.d/my_policy') do
+    it { should_not exist }
+  end
+end
+
+control 'apparmor-policy-remove-03' do
+  impact 1.0
+  title 'Policy is not loaded in AppArmor'
+
+  describe command('apparmor_status') do
+    its('stdout') { should_not match %r{/usr/sbin/my_policy} }
+  end
+end
